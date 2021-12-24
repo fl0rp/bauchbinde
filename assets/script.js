4cfb9c3e570c1c2d11f69dcf25b777dbd62a2459
Florian Sorg <florian@sorg.tech>
2021-12-24 22:33:14 +0100

    implement basic rc3 nowhere style

--- assets/script.js
+++ assets/script.js
@@ -53,8 +53,6 @@
 
     async function init() {
         const root = document.querySelector('html');
-        textEl = document.querySelector('.text');
-        textEl.innerHTML = '';
 
         if (window.location.search) {
             const pairs = window.location.search.slice(1).split('&').map(x => x.split('='));
@@ -126,7 +124,7 @@
             } else {
                 headline = talk.title;
                 if (talk.persons) {
-                    speaker = talk.persons.map(person => person.public_name).join(' / ');
+                    speaker = talk.persons.map(person => person.public_name).join(', ');
                 }
             }
         }
@@ -135,164 +133,28 @@
             return false;
         }
 
-        if (speaker && headline) {
-            headline += ',';
-        }
+        const headlineEl = document.querySelector('.headline');
+        headlineEl.innerHTML = '';
+        const speakerEl = document.querySelector('.speaker');
+        speakerEl.innerHTML = '';
 
-        const headlineEl = document.createElement('span');
-        headlineEl.classList.add('headline');
-        const speakerEl = document.createElement('span');
-        speakerEl.classList.add('speaker');
+        console.log('headline', headline);
+        console.log('speaker', speaker);
 
         if (headline) {
-            Array.from(headline).forEach(letter => {
-                const letterEl = document.createElement('span');
-                letterEl.classList.add('letter');
-                letterEl.innerText = letter;
-                headlineEl.appendChild(letterEl);
-            })
+            headlineEl  .innerText = headline;
         }
         if (speaker) {
-            Array.from(speaker).forEach(letter => {
-                const letterEl = document.createElement('span');
-                letterEl.classList.add('letter');
-                letterEl.innerText = letter;
-                speakerEl.appendChild(letterEl);
-            })
-        }
-
-        textEl.appendChild(headlineEl);
-        textEl.appendChild(document.createTextNode(' '));
-        textEl.appendChild(speakerEl);
-
-        const mainTilesEl = document.querySelector('.background .main-tiles');
-        mainTilesEl.innerHTML = '';
-
-        for (let i = 0; i < 16; i++) {
-            const tile = document.createElement('div');
-            tile.classList.add('tile', 'large');
-            mainTilesEl.appendChild(tile);
-        }
-
-        const secondaryTilesEl = document.querySelector('.background .secondary-tiles');
-        secondaryTilesEl.innerHTML = '';
-
-        for (let i = 0; i < 17; i++) {
-            const tile = document.createElement('div');
-            tile.classList.add('tile');
-            if (i === 0 || Math.random() > 0.5) {
-                tile.classList.add('medium');
-            } else {
-                tile.classList.add('small');
-            }
-            secondaryTilesEl.appendChild(tile);
+            speakerEl.innerText = speaker;
         }
 
         return true;
     }
 
     async function animate() {
-        if (autoIntro ? isFirstRun : isIntro) {
-            await Promise.all([
-                slideIn(),
-                fadeInText(),
-            ]);
-        } else {
-            await Promise.all([
-                fadeIn(),
-                fadeInText(),
-            ]);
-        }
+        document.querySelector('.container').classList.add('visible');
         await new Promise(r => setTimeout(r, holdDuration * 1000));
-        await Promise.all([
-            fadeOut(),
-            fadeOutText(),
-        ])
-    }
-
-    async function slideIn() {
-        const tiles = document.querySelectorAll('.tile');
-
-        for (const tile of tiles) {
-            tile.style.animationName = `slide-in-${Math.floor(Math.random() * 10)}`;
-            tile.style.animationDelay = `${Math.random() * 0.4}s`;
-            tile.style.animationDuration = `${0.8 + Math.random() * 0.4}s`;
-            tile.style.animationIterationCount = '1';
-            tile.style.animationTimingFunction = 'ease-in';
-        }
-        await new Promise(r => setTimeout(r, 600));
-        for (const tile of tiles) {
-            tile.classList.add('visible');
-        }
-        await new Promise(r => setTimeout(r, 1000));
-    }
-
-    async function fadeIn() {
-        const tiles = document.querySelectorAll('.tile');
-
-        for (const tile of tiles) {
-            tile.style.animationName = `fade-in`;
-            tile.style.animationDelay = `${Math.random() * 0.4}s`;
-            tile.style.animationDuration = `${0.8 + Math.random() * 0.4}s`;
-            tile.style.animationIterationCount = '1';
-            tile.style.animationTimingFunction = 'ease-in';
-        }
-        await new Promise(r => setTimeout(r, 600));
-        for (const tile of tiles) {
-            tile.classList.add('visible');
-        }
-        await new Promise(r => setTimeout(r, 1000));
-    }
-
-    async function fadeOut() {
-        const tiles = document.querySelectorAll('.tile');
-
-        for (const tile of tiles) {
-            tile.style.animationName = `fade-out`;
-            tile.style.animationDelay = `${Math.random() * 0.4}s`;
-            tile.style.animationDuration = `${0.8 + Math.random() * 0.4}s`;
-            tile.style.animationIterationCount = '1';
-            tile.style.animationTimingFunction = 'ease-in';
-        }
-        await new Promise(r => setTimeout(r, 600));
-        for (const tile of tiles) {
-            tile.classList.remove('visible');
-        }
-        await new Promise(r => setTimeout(r, 1000));
-    }
-
-    async function fadeInText() {
-        const letters = document.querySelectorAll('.letter');
-
-        for (const letter of letters) {
-            letter.style.animationName = `fade-in`;
-            letter.style.animationDelay = `${Math.random() * 0.4}s`;
-            letter.style.animationDuration = `${0.8 + Math.random() * 0.4}s`;
-            letter.style.animationIterationCount = 1;
-            letter.style.animationTimingFunction = 'ease-in';
-        }
-        await new Promise(r => setTimeout(r, 600));
-        for (const tile of letters) {
-            tile.classList.add('visible');
-        }
-        await new Promise(r => setTimeout(r, 1000));
-    }
-
-    async function fadeOutText() {
-        const letters = document.querySelectorAll('.letter');
-
-        for (const letter of letters) {
-            letter.style.animationName = `fade-out`;
-            letter.style.animationDelay = `${Math.random() * 0.4}s`;
-            letter.style.animationDuration = `${0.8 + Math.random() * 0.4}s`;
-            letter.style.animationIterationCount = '1';
-            letter.style.animationTimingFunction = 'ease-in';
-        }
-        await new Promise(r => setTimeout(r, 600));
-        for (const tile of letters) {
-            tile.classList.remove('visible');
-        }
-        await new Promise(r => setTimeout(r, 1000));
+        document.querySelector('.container').classList.remove('visible');
     }
 
     async function cycle() {
